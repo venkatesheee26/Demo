92655dbc2e3efeda82e7118c62ee5d84c23c12b6
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-09-20 12:02:57 +0530

    ps

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
